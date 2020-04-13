b976cab8ff414fe02a5c59b2d122ab9ead2a8235
Yusuke Sato <yusuke_s@YusukenoMacBook-ea.local>
2020-04-13 13:34:42 +0900

    Create index function

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    public function index()
+    {
+        // dummy data
+        $item = [
+          (object) [
+              'id' => 1,
+              'name' => 'sample1',
+              'created_at' => now()
+          ],
+          (object) [
+              'id' => 2,
+              'name' => 'sample2'
+              'created_at' => now()
+          ],
+        ];
+
+        return view('item.index', ['item' => $item]);
+    }
 }
